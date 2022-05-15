a0bef18dcdca27f7b753297e6f181f1bab33cfbb
Andrew1013-development <73055116+Andrew1013-development@users.noreply.github.com>
2022-05-15 17:19:04 +0700

    Update pCLI to v0.0.0a3
    Changelog :
    - added assignvar command
    - added compile_tag parameter
    - changed ver function
    - removed flag conditional check 
    - fixed a critical bug in v0.0.0a2 : var_type getting assigned as var_name when using definevar

--- main.py
+++ main.py
@@ -1,11 +1,13 @@
 # dependencies
 from datetime import date
+#from termcolor import cprint
 
 # command pallete
 # ver : print version
 # printstr [string] : print string
 # printvar [variable name] : print variable value
-# definevar [variable name] [variable type] [variable name] : define a varaible with valur
+# definevar [variable name] [variable type] [variable value] : define a varaible with value of selected type
+# assignvar [variable name] [value type] [new variable value] : assign a given variable a new value of selected type
 # exit : exit cli
 
 # variable types
@@ -18,10 +20,11 @@ major = 0
 minor = 0
 rev = 0
 branch = "a"
-build = 2
-flag = "C2"
-flag_desc = "CONCEPTION 2"
-compiled_date = date(2022,5,12)
+build = 3
+flag = "C3"
+flag_desc = "CONCEPTION 3"
+compiled_date = date(2022,5,15)
+compile_tag = "0515"
 version_string = f"v{major}.{minor}.{rev}{branch}{build}"
 
 #variables
@@ -33,13 +36,13 @@ while userin != "exit" :
     userin = input()
     if userin == "ver" :
         print(f"Python CLI version {version_string}")
-        print(f"Compiled on {compiled_date}")
+        print(f"Compiled on {compiled_date} with compilation tag {compile_tag}")
         if flag != "" :
             print(f"Flags for program : {flag} ({flag_desc})")
     elif userin == "exit" :
         break
     elif "definevar" in userin :
-        var_type,var_name,var_value = [s for s in userin.removeprefix("definevar ").split(" ")]
+        var_name,var_type,var_value = [s for s in userin.removeprefix("definevar ").split(" ")]
         #type determination
         if var_type == "int" :
             var_value = int(var_value)  
@@ -48,6 +51,19 @@ while userin != "exit" :
         elif var_type == "bool" :
             var_value = bool(var_value)
         variables[var_name] = var_value #assign variable and value
+    elif "assignvar" in userin :
+        var_type,var_name,var_value = [s for s in userin.removeprefix("definevar ").split(" ")]
+        #type determination
+        if var_type == "int" :
+            var_value = int(var_value)  
+        elif var_type == "float" :
+            var_value = float(var_value)
+        elif var_type == "bool" :
+            var_value = bool(var_value)
+        try :
+            variables[var_name] = var_value #assign variable and value
+        except KeyError :
+            print(f"No variable name : {var_name}")
     elif "printstr" in userin :
         print([s for s in userin.split(" ")][1])
     elif "printvar" in userin :
