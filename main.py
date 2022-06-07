854fed74bff453471506cf0aa731187092c7c689
Andrew1013-development <73055116+Andrew1013-development@users.noreply.github.com>
2022-06-07 18:48:16 +0700

    updated main.py to v0.0.0a5
    Changelog:
    - from v0.0.0b1-0528
      - fixed bugs for printc and printstr commands when printing multiple spaces strings (resolved #5, resolved #6)
      - fixed bugs of printvar causing the CLI to exit when printing a non-existing variable (resolved #7)
      - added "> " in front of command input
      - changed error output message for assignvar command
      - added code to terminate on KeyboardInterrupt exception
    - new :
      - added new dependencies : python_version, release, system with platform
      - added new information when using ver : OS name / OS release, OS version, Python version the CLI is run on
      - changed the syntax of definevar : type goes before the name

--- main.py
+++ main.py
@@ -1,40 +1,43 @@
-#requirements : Python 3.9+ 
-#tested on Python 3.10.4 and Python 3.11.0a7
-
 # dependencies
 from datetime import date
-from datetime import datetime   
-#from termcolor import cprint as printc
-import time
+from termcolor import cprint as printc
+from datetime import datetime
+from time import localtime
+from platform import python_version
+from platform import release
+from platform import system
+from platform import platform
+import os
+import sys
 
 # command pallete
 # ver : print version
-# printstr [string] : print string
-# printvar [variable name] : print variable value
-# definevar [variable name] [variable type] [variable value] : define a varaible with value of selected type
-# assignvar [variable name] [value type] [new variable value] : assign a given variable a new value of selected type
-# printc [string] [color] : print string with colors
-# date : print current date
-# time : print current time
+# printstr [string] : print string 
+# printvar [variable name] : print variable value (if no variable name is specified, printvar will print all variables available)
+# definevar [variable type] [varianle type] [variable value] : define a varaible with value of selected type
+# assignvar [variable name] [variable type] [new variable value] : assign a given variable a new value of selected type
+# clearvar [variable name] : clear variable's value (reset value to blank)
 # exit : exit cli
 
-
 # variable types
-# int, float : numbers
+# integer, float : numbers
 # string (default type)
-# bool : True / False
+# boolean : True / False
 
-#version shits
+#version thingies
 major = 0
 minor = 0
 rev = 0
 branch = "a"
-build = 4
-flag = "C4"
-flag_desc = "CONCEPTION 4"
-compiled_date = date(2022,5,17)
-compile_tag = "0517"
+build = 5
+flag = "C5"
+flag_desc = "CONCEPTION 5"
+compiled_date = date(2022,6,7)
+compile_tag = "0607"
 version_string = f"v{major}.{minor}.{rev}{branch}{build}"
+python_version = python_version()
+os_release = system() + " " + release()
+os_version = platform().replace("-"," ",2)
 
 #variables
 userin = ""
@@ -42,51 +45,71 @@ variables = {}
 
 #main code
 while userin != "exit" :
-    userin = input()
-    if userin == "ver" :
-        print(f"Python CLI version {version_string}")
-        print(f"Compiled on {compiled_date} with compilation tag {compile_tag}")
-        if flag != "" :
-            print(f"Flags for program : {flag} ({flag_desc})")
-    elif userin == "exit" :
-        break
-    elif userin == "date" :
-        print(datetime.now().strftime("%H:%M:%S"))
-    elif userin == "time" :
-        print(time.strftime("%H:%M:%S", time.localtime()))
-    elif "definevar" in userin :
-        var_name,var_type,var_value = [s for s in userin.removeprefix("definevar ").split(" ")]
-        #type determination
-        if var_type == "int" :
-            var_value = int(var_value)  
-        elif var_type == "float" :
-            var_value = float(var_value)
-        elif var_type == "bool" :
-            var_value = bool(var_value)
-        variables[var_name] = var_value #assign variable and value
-    elif "assignvar" in userin :
-        var_type,var_name,var_value = [s for s in userin.removeprefix("definevar ").split(" ")]
-        #type determination
-        if var_type == "int" :
-            var_value = int(var_value)  
-        elif var_type == "float" :
-            var_value = float(var_value)
-        elif var_type == "bool" :
-            var_value = bool(var_value)
-        try :
+    try :
+        userin = input("> ")
+        if userin == "ver" :
+            print(f"Python CLI version {version_string}")
+            print(f"Compiled on {compiled_date} with compilation tag {compile_tag}")
+            if flag != "" :
+                print(f"Flags for program : {flag} ({flag_desc})")
+            print(f"Ran on Python version {python_version}")
+            print(f"Operating system : {os_release}")
+            print(f"OS Version : {os_version}")
+        elif userin == "exit" :
+            print("Exiting...")
+            break
+        elif "definevar" in userin :
+            var_type,var_name,var_value = [s for s in userin.removeprefix("definevar ").split(" ")]
+            #type determination
+            if var_type == "integer" :
+                var_value = int(var_value)  
+            elif var_type == "float" :
+                var_value = float(var_value)
+            elif var_type == "boolean" :
+                var_value = bool(var_value)
             variables[var_name] = var_value #assign variable and value
-        except KeyError :
-            print(f"No variable name : {var_name}")
-    elif "printstr" in userin :
-        print([s for s in userin.split(" ")][1])
-    elif "printvar" in userin :
-        print(variables[[s for s in userin.split(" ")][1]])
-    else :
-        print("Error : Invaild command")
-    '''
-    elif "printc" in userin :
-        string,color = [s for s in userin.removeprefix("printc ").split(" ")]
-        printc(string,color)
-    '''
-    
-    print() #print empty line
+        elif "assignvar" in userin :
+            var_name,var_type,var_value = [s for s in userin.removeprefix("definevar ").split(" ")]
+            #type determination
+            if var_type == "integer" :
+                var_value = int(var_value)  
+            elif var_type == "float" :
+                var_value = float(var_value)
+            elif var_type == "boolean" :
+                var_value = bool(var_value)
+            try :
+                variables[var_name] = var_value #assign variable and value
+            except KeyError :
+                print(f"Variable {var_name} does not exist!")
+        elif "clearvar" in userin :
+            var_name = [s for s in userin.split(" ")][1]
+            del variables[var_name]
+        elif "printstr" in userin :
+            printstr_input = [s for s in userin.removeprefix("printstr ").split(" ")]
+            string = " ".join(printstr_input)
+        elif "printvar" in userin :
+            if userin == "printvar" :
+                if variables == {} :
+                    print("There's no varaibles")
+                else :
+                    print("currently available variables :")
+                    for key in variables :  
+                        print(f"{key} <- {variables[key]} (type : {type(variables[key])})")
+            else :
+                var_name = [s for s in userin.split(" ")][1]
+                if var_name in variables.keys() :
+                    print(variables[var_name])
+                else :
+                    print(f"Variable {var_name} does not exist!")
+        elif "printc " in userin :
+            printc_input = [s for s in userin.removeprefix("printc ").split(" ")]
+            color = printc_input[len(printc_input)-1]
+            printc_input.remove(color)
+            string = " ".join(printc_input)
+            printc(string, color)
+        else :
+            print("Error : Invaild command")
+        print() #print empty line
+    except KeyboardInterrupt :
+        print("\nExiting from Ctrl-C trigger...")
+        break
